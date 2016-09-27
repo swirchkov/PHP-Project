06dc2b9f524f56c8a44813d25216e88a7d58daea
Wladimir Swirchkov <swirchkov@gmail.com>
2016-09-27 17:54:17 +0300

    design of login page is finished

---
+++ shared/header.js
@@ -0,0 +1,23 @@
+
+function hideSearchlink() {
+    document.getElementsByClassName('search-btn')[0].style.display = 'none';
+}
+
+function showSearchLink() {
+    document.getElementsByClassName('search-btn')[0].style.display = 'inline-block';
+}
+
+document.onload = function (e) {
+    hideSearchlink();
+
+    document.getElementsByClassName('search-query')[0].addEventListener('input', function(e) {
+
+        if (e.srcElement.value.trim() !== "") { 
+            showSearchLink();
+        }
+        else {
+            hideSearchlink();
+        }
+
+    });
+}
