509fecddaee3be505e1e2145308a1c770d9cda2e
Wladimir Swirchkov <swirchkov@gmail.com>
2016-10-11 07:59:55 +0300

    added functionality of storing user in localstorage and some formating for article view

--- angular-app/app/session.js
+++ angular-app/app/session.js
@@ -1,10 +1,31 @@
 "use strict";
+// models
+var user_1 = require("./models/user");
 var Session = (function () {
     function Session() {
     }
     Object.defineProperty(Session, "AuthenticatedUser", {
-        get: function () { return this.authenticatedUser; },
-        set: function (value) { this.authenticatedUser = value; },
+        get: function () {
+            var user = this.authenticatedUser;
+            if (user == null) {
+                var store = localStorage.getItem('User');
+                var parseUser = JSON.parse(store);
+                if (parseUser == null) {
+                    return null;
+                }
+                else {
+                    return new user_1.User(parseUser.login, parseUser.email, parseUser.password, parseUser.imageSrc, parseUser.imageId, parseUser.id);
+                }
+            }
+            return this.authenticatedUser;
+        },
+        set: function (value) {
+            this.authenticatedUser = value;
+            if (value == null) {
+                localStorage.removeItem('User');
+            }
+            localStorage.setItem('User', JSON.stringify(value));
+        },
         enumerable: true,
         configurable: true
     });
--- angular-app/app/session.ts
+++ angular-app/app/session.ts
@@ -4,7 +4,30 @@ import { User } from "./models/user";
 export class Session {
     private static authenticatedUser : User = null;
 
-    public static get AuthenticatedUser() { return this.authenticatedUser; }
-    public static set AuthenticatedUser(value: User) { this.authenticatedUser = value; }; 
+    public static get AuthenticatedUser() { 
+        var user = this.authenticatedUser;
+        if (user == null) {
+            var store = localStorage.getItem('User');
+            var parseUser = JSON.parse(store);
+
+            if (parseUser == null) {
+                return null;
+            }
+            else {
+                return new User(parseUser.login, parseUser.email, parseUser.password, parseUser.imageSrc, parseUser.imageId, 
+                            parseUser.id);
+            }
+        }
+        return this.authenticatedUser; 
+    }
+    public static set AuthenticatedUser(value: User) { 
+        this.authenticatedUser = value; 
+
+        if (value == null) {
+            localStorage.removeItem('User');
+        }
+
+        localStorage.setItem('User', JSON.stringify(value));
+    }; 
     
 }
